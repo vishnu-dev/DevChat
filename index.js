2794b2eacf6ef701410b36667ea33c7dfa4eb698
vishnu-dev <thevishnudev@gmail.com>
2017-07-08 06:32:54 -0700

    Signup page

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var exphbs  = require('express-handlebars');
 var mongoose = require('mongoose');
 var bparse = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var passportLocalMongoose = require('passport-local-mongoose');
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -15,8 +19,10 @@ app.set('view engine', 'handlebars');
 app.use(bparse.urlencoded({ extended: false }));
 app.use(bparse.json());
 
+// Static files
 app.use(express.static(__dirname + '/public'));
 
+// MongoDB Database init
 mongoose.connect('mongodb://127.0.0.1:27017/chat-app');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -25,14 +31,42 @@ db.once('open', function() {
 });
 
 // User Collection
+var UserSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
+var User = mongoose.model('user', UserSchema);
+
+User.plugin(passportLocalMongoose);
+
+// Message Collection
 var MessageSchema = mongoose.Schema({
     msg:Object
 });
 var Msg = mongoose.model('msg', MessageSchema);
 
+// passport config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get('/', function(req, res){
-  res.redirect('/chat');
+  res.render('home');
 });
+
+app.post('/',function(req,res) {
+	if(req.body.flag==1){
+		User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+	        if (err) {
+	            return res.render('register', { account : account });
+	        }
+
+	        passport.authenticate('local')(req, res, function () {
+	            res.redirect('/chat');
+	        });
+	    });
+	}
+})
 
 io.on('connection', function(socket){
 	console.log('connected');
