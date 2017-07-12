2202b187f9e5df4ba625907315e1ab50631760d0
vishnu-dev <thevishnudev@gmail.com>
2017-07-12 20:47:29 +0530

    server changed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bparse.json());
 app.use(express.static(__dirname + '/public'));
 
 // MongoDB Database init
-mongoose.connect('mongodb://127.0.0.1:27017/chat-app',{useMongoClient: true});
+mongoose.connect('mongodb://139.59.74.8:27017/chat-app',{useMongoClient: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
